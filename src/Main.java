f5720a8f26a64143baa0bb8db813a77c2472eea6
aren.bronton24 <aren.bronton24@d155.org>
2023-02-15 11:30:01 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.add(new GamePanel());
+
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
